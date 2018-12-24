81386f58eab7ed92ed712092423e407a62a8584c
KhanShaheb34 <shakirul34@student.sust.edu>
2018-12-24 12:28:06 +0600

    Minor change

--- login/main.js
+++ login/main.js
@@ -1,5 +1,7 @@
 
 function facultyClicked(cb) {
+    document.getElementsByName('regno')[0].value = "";
+    document.getElementsByName('pass')[0].value = "";
     if (cb.checked) {
         document.getElementsByName('regno')[0].placeholder="Email";
     }
