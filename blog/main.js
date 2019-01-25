71e9194015c05604b7491abd852d81d5930f5346
Shakirul Hasan <theshk.me@gmail.com>
2019-01-25 22:34:22 +0600

    Blog Template Created

---
+++ blog/main.js
@@ -0,0 +1,12 @@
+window.onscroll = function() { scrollAction() };
+
+function scrollAction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementsByClassName('tab-section')[0].style.position = 'fixed';
+        document.getElementsByClassName('tab-section')[0].style.marginTop = '-80px'
+    }
+    else{
+        document.getElementsByClassName('tab-section')[0].style.position = 'relative';
+        document.getElementsByClassName('tab-section')[0].style.marginTop = '80px'
+    }
+}
